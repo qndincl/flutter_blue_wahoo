f461af1c0268b2a84dbd150c36835e95a16a9faa
qndincl <39212474+qndincl@users.noreply.github.com>
2021-04-18 16:53:13 +0900

    Add binding -  <BluetoothController>

---
+++ lib/binding.dart
@@ -0,0 +1,12 @@
+import 'package:flutter_blue_project/controller/bluetooth_controller.dart';
+import 'package:get/get.dart';
+
+class HomeBinding implements Bindings {
+  @override
+  void dependencies() {
+    // Get.lazyPut<HomeController>(() => HomeController());
+    // Get.put<Service>(()=> Api());
+    // Get.
+    Get.lazyPut<BluetoothController>(() => BluetoothController());
+  }
+}
